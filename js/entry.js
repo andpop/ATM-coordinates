96d9f7e1f8199a0592fecd21c329f456416b0e9e
Andrey Popov <andpop@mail.ru>
2018-12-23 23:34:19 +0300

    Resolve coordinates, but async error.

--- js/entry.js
+++ js/entry.js
@@ -1,6 +1,42 @@
+import sourceAtmAddresses from '../data/atm_adresses';
+
 ymaps.ready(init);
 
+function addCoordsToAtm(atm, index) {
+    let atmWithCoords = {};
+
+    atmWithCoords.id = atm.ID;
+    atmWithCoords.address = atm.address;
+    atmWithCoords.place = atm.place;
+    atmWithCoords.category = atm.category;
+    ymaps.geocode(`${atm.city},${atm.address}`)
+        .then(res => {
+            let firstGeoObject = res.geoObjects.get(0),
+                coords = firstGeoObject.geometry.getCoordinates(),
+                address = firstGeoObject.getAddressLine();
+
+            atmWithCoords.normalAddress = address;
+            atmWithCoords.latitude = coords[0];
+            atmWithCoords.longitude = coords[1];
+        })
+        .catch(err => console.error(`${index}: ${atm.full_address}: ${err}`));
+
+    return atmWithCoords;
+}
+
+function makeAtmLi(atmList) {
+    let atmLi = '';
+    atmList.forEach(atm => {
+        console.log(atm);
+        console.log(atm["normalAddress"]);
+        atmLi += `<li>${atm.id};${atm.normalAddress};${atm.latitude};${atm.longitude}</li>`;
+    });
+
+    return atmLi;
+}
+
 function init () {
+    let atmList = document.getElementById('atm_list');
 
     // =========================================================================
     let myMap = new ymaps.Map('map', {
@@ -9,19 +45,50 @@ function init () {
         behaviors: ['drag']
     });
     myMap.controls.add('zoomControl');
+    let clusterer = new ymaps.Clusterer({
+        preset: 'islands#invertedDarkOrangeClusterIcons',
+        openBalloonOnClick: true,
+        clusterDisableClickZoom: true,
+        clusterOpenBalloonOnClick: true,
+        clusterHideIconOnBalloonOpen: false,
+        // Устанавливаем для балуна кластера стандартный макет типа "Карусель".
+        clusterBalloonContentLayout: 'cluster#balloonCarousel',
+        // Устанавливаем собственный макет.
+        // clusterBalloonItemContentLayout: customItemContentLayout,
+        // Устанавливаем режим открытия балуна.
+        // В данном примере балун никогда не будет открываться в режиме панели.
+        clusterBalloonPanelMaxMapArea: 0,
+        // Устанавливаем размеры макета контента балуна (в пикселях).
+        clusterBalloonContentLayoutWidth: 200,
+        clusterBalloonContentLayoutHeight: 130,
+        // Устанавливаем максимальное количество элементов в нижней панели на одной странице
+        clusterBalloonPagerSize: 5
+    });
+    myMap.geoObjects.add(clusterer);
 
-    ymaps.geocode('Саранск, ул. Степана Разина,17, Регистрационная палата')
-        .then(res => {
-            let firstGeoObject = res.geoObjects.get(0),
-                coords = firstGeoObject.geometry.getCoordinates(),
-                address = firstGeoObject.getAddressLine();
+    let targetAtmList = sourceAtmAddresses.map((atm, index) => addCoordsToAtm(atm, index));
+    // console.log("target=", targetAtmList);
+    atmList.innerHTML = makeAtmLi(targetAtmList);
 
-            console.log(coords);
-            console.log(address);
-            // currentReview.coords = coords;
-            // currentReview.address = res.geoObjects.get(0).getAddressLine();
-            // showForm(position, currentReview.address);
-        })
-        .catch(err => console.error(err));
+    // ymaps.geocode('Саранск, ул. Степана Разина,17, Регистрационная палата')
+    // ymaps.geocode('Саранск, ул. Степана Разина,17, Регистрационная палата')
+    //     .then(res => {
+    //         let firstGeoObject = res.geoObjects.get(0),
+    //             coords = firstGeoObject.geometry.getCoordinates(),
+    //             address = firstGeoObject.getAddressLine();
+    //
+    //         let placemark = new ymaps.Placemark(coords, {
+    //             balloonContentHeader: `${address}`,
+    //             // balloonContentBody: text,
+    //             // balloonContentFooter: date,
+    //             hintContent: `12345`
+    //         }, {
+    //             preset: 'islands#redIcon',
+    //             iconColor: '#df6543',
+    //             openBalloonOnClick: false
+    //         });
+    //         clusterer.add(placemark);
+    //     })
+    //     .catch(err => console.error(err));
 
 }
